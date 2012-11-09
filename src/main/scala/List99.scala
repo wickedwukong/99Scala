9e790a81db866de85583f9f8413226f8f81cdc9f
Xuemin Guan <gxuemin@gmail.com>
2012-11-09 16:33:23 +0000

    added length and nth tests

--- src/main/scala/List99.scala
+++ src/main/scala/List99.scala
@@ -14,4 +14,17 @@ object List99 {
       case _ => throw new Error("Empty list")
     }
   }
+
+  def nth[T](index: Int, list: List[T]): T = {
+    (index, list) match {
+      case (0, h :: _) => h
+      case (n, h :: tail) => nth(n - 1, tail)
+      case (_, Nil) => throw new Error("Empty list")
+    }
+  }
+
+  def length[T](list: List[T]): Int = {
+    list.foldLeft(0)((count, elem) => count + 1)
+
+  }
 }
--- src/test/scala/List99Spec.scala
+++ src/test/scala/List99Spec.scala
@@ -41,4 +41,56 @@ class List99Spec extends Specification {
     }
   }
 
+  "nth with parameter 0" should {
+    "return the first element in a 1 element list" in {
+      List99.nth(0, List(1)) must_== 1
+    }
+
+    "return the first element in a 2 element list" in {
+      List99.nth(0, List("a", "b")) must_== "a"
+    }
+
+    "throw exception for 0 element list" in {
+      List99.nth(0, List[Any]()) must throwAn[Error]
+    }
+
+
+  }
+
+  "nth with parameter 1" should {
+    "return the last element in a 2 element list" in {
+      List99.nth(1, List(1, 2)) must_== 2
+    }
+
+    "return the second element in a 3 element list" in {
+      List99.nth(1, List("a", "b", "c")) must_== "b"
+    }
+
+    "throw exception for 1 element list" in {
+      List99.nth(1, List(1)) must throwAn[Error]
+    }
+  }
+
+  "length" should {
+    "return the 0 for an empty list" in {
+      List99.length(List()) must_== 0
+    }
+
+    "return 1 for a 1 element list" in {
+      List99.length(List("c")) must_== 1
+    }
+
+    "return 2 for a 2 element list" in {
+      List99.length(List("a", "c")) must_== 2
+    }
+
+    "return 3 for a 3 element list" in {
+      List99.length(List("a","b", "c")) must_== 3
+    }
+//
+//    "throw exception for 1 element list" in {
+//      List99.nth(1, List(1)) must throwAn[Error]
+//    }
+  }
+
 }
