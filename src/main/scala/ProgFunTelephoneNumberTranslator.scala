564bce4e3bf58107d08ae97031eb456461e19ddf
Xuemin Guan <gxuemin@gmail.com>
2012-11-11 15:37:47 +0000

    adding for expression solution for the charCode

--- src/main/scala/ProgFunTelephoneNumberTranslator.scala
+++ src/main/scala/ProgFunTelephoneNumberTranslator.scala
@@ -2,7 +2,7 @@ object ProgFunTelephoneNumberTranslator {
   val mnem = Map('2' -> "ABC", '3' -> "DEF", '4' -> "GHI", '5' -> "JKL", '6' -> "MNO", '7' -> "PQRS", '8' -> "TUV", '9' -> "WXYZ")
   //    val mnem = Map('2' -> "ABC", '3' -> "DEF", '4' -> "GHI", '5' -> "JKL", '6' -> "MNO", '7' -> "PQRS", '8' -> "TUV", '9' -> "WXYZ")
 
-  val charCode: Map[Char, Char] = {
+  val charCodeFoldLeft: Map[Char, Char] = {
     mnem.foldLeft(Map[Char, Char]())(
       (seed, elem) => {
         elem._2.foldLeft(seed)((seed, c) => {
@@ -10,6 +10,10 @@ object ProgFunTelephoneNumberTranslator {
         })
       }
     )
+  }
+
+  val charCodeFor: Map[Char, Char] = {
+    for ((number, strValue) <- mnem; charValue <- strValue) yield charValue -> number
   }
 }
 
--- src/test/scala/ProgFunTelephoneNumberTranslatorSpec.scala
+++ src/test/scala/ProgFunTelephoneNumberTranslatorSpec.scala
@@ -9,9 +9,17 @@ import ProgFunTelephoneNumberTranslator._
 @RunWith(classOf[JUnitRunner])
 class ProgFunTelephoneNumberTranslatorSpec extends Specification {
 
-  "invertMap" should {
-    "char to number char mapping" in {
-      charCode must_== Map('A' -> '2', 'B' -> '2', 'C' -> '2', 'D' -> '3', 'E' -> '3', 'F' -> '3', 'G' -> '4', 'H' -> '4', 'I' -> '4', 'J' -> '5', 'K' -> '5', 'L' -> '5', 'M' -> '6', 'N' -> '6', 'O' -> '6',
+  "charCodeFoldLeft" should {
+    "give char to number char mapping" in {
+      charCodeFoldLeft must_== Map('A' -> '2', 'B' -> '2', 'C' -> '2', 'D' -> '3', 'E' -> '3', 'F' -> '3', 'G' -> '4', 'H' -> '4', 'I' -> '4', 'J' -> '5', 'K' -> '5', 'L' -> '5', 'M' -> '6', 'N' -> '6', 'O' -> '6',
+      'P' -> '7', 'Q' -> '7', 'R' -> '7', 'S' -> '7', 'T' -> '8', 'U' -> '8', 'V' -> '8', 'W' -> '9', 'X' -> '9', 'Y' -> '9', 'Z' -> '9')
+
+    }
+  }
+
+  "charCodeFor" should {
+    "give char to number char mapping" in {
+      charCodeFor must_== Map('A' -> '2', 'B' -> '2', 'C' -> '2', 'D' -> '3', 'E' -> '3', 'F' -> '3', 'G' -> '4', 'H' -> '4', 'I' -> '4', 'J' -> '5', 'K' -> '5', 'L' -> '5', 'M' -> '6', 'N' -> '6', 'O' -> '6',
       'P' -> '7', 'Q' -> '7', 'R' -> '7', 'S' -> '7', 'T' -> '8', 'U' -> '8', 'V' -> '8', 'W' -> '9', 'X' -> '9', 'Y' -> '9', 'Z' -> '9')
 
     }
