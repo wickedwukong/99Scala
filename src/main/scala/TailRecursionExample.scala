d7b4c29e12016be6986d3dc04fc00ad29236abdc
wickedwukong <gxuemin@gmail.com>
2013-02-13 18:15:08 +0000

    added an tail rec example. update or add new elememnt to a list.

--- src/main/scala/TailRecursionExample.scala
+++ src/main/scala/TailRecursionExample.scala
@@ -1,25 +1,22 @@
-package features
-
-import org.specs2.mutable._
 import annotation.tailrec
 
 object TailRecursionExample {
 
-  def updateList(newValue: Int, oldList: List[Int]): List[Int] = {
+  def updateOrAddNewElementToList(newValue: Int, oldList: List[Int]): List[Int] = {
     @tailrec
-    def foo(newValue: Int, acc: List[Int], oldList: List[Int]): List[Int] = {
+    def recursivelyAddUpdateOrAddNewValueToList(newValue: Int, acc: List[Int], oldList: List[Int]): List[Int] = {
       oldList match {
         case head :: tail => {
           if (head == newValue) {
             (((head + 1) :: tail).reverse ++ acc).reverse
           } else {
-            foo(newValue, head :: acc, tail)
+            recursivelyAddUpdateOrAddNewValueToList(newValue, head :: acc, tail)
           }
         }
         case Nil => (newValue :: acc).reverse
       }
     }
-    foo(newValue, List(), oldList)
+    recursivelyAddUpdateOrAddNewValueToList(newValue, List(), oldList)
   }
 
 }
--- src/test/scala/TailRecursionExampleSpec.scala
+++ src/test/scala/TailRecursionExampleSpec.scala
@@ -1,3 +1,19 @@
-class TailRecursionExampleSpec {
+import org.specs2.mutable.Specification
+import TailRecursionExample._
 
+class TailRecursionExampleSpec extends Specification {
+
+  "updateOrAddNewElementToList" should {
+    "add new element to the end of list" in {
+      updateOrAddNewElementToList(5, List(1, 2, 3)) must_== List(1, 2, 3, 5)
+    }
+  }
+
+  "updateOrAddNewElementToList" should {
+    "update the existing element and keep the original order" in {
+      updateOrAddNewElementToList(2, List(1, 2, 3)) must_== List(1, 3, 3)
+      updateOrAddNewElementToList(3, List(1, 2, 3)) must_== List(1, 2, 4)
+      updateOrAddNewElementToList(1, List(1, 2, 3)) must_== List(2, 2, 3)
+    }
+  }
 }
