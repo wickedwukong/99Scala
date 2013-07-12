6f9008409e0e28e055fd1dad8e3e6f6bda8653d6
Xuemin Guan <xuemin.guan@db.com>
2013-07-12 16:04:19 +0100

    adding a bit more monad

--- src/main/scala/monad/EitherMonad.scala
+++ src/main/scala/monad/EitherMonad.scala
@@ -19,6 +19,21 @@ object EitherMonad {
   implicit def toEitherMonad[A, B](a: Either[A, B]): EitherMonad[A, B] = EitherMonad(a)
 
   def sequence[A, B](eithers: Seq[Either[A, B]]): Either[A, Seq[B]] = {
+//    val s1: Either[A, Seq[B]] = for {
+//      r0 <- eithers(0)
+//    } yield Seq(r0)
+//
+//    val s2: Either[A, Seq[B]] = for {
+//      rights <- s1
+//      r1 <- eithers(1)
+//    } yield rights :+ r1
+//
+//    val s3: Either[A, Seq[B]] = for {
+//      rights <- s2
+//      r2 <- eithers(2)
+//    } yield rights :+ r2
+
+
     eithers.foldLeft(Right(Seq()): Either[A, Seq[B]]){
       (acc: Either[A, Seq[B]], elem: Either[A, B]) =>
         for {
